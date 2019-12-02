c25fb5202e151b420642992162c8fef23876f670
bradamorg007 <bradley.morgan190@gmail.com>
2019-12-02 12:54:55 +0000

    Adding The body, header and footer templates

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <div className={css.Footer}>
+            <span>Footer</span>
+        </div>
+
+    )
+};
+
+export default Footer
